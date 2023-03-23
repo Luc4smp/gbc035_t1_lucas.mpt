12b8ec6b4709fb512591620a86e6535a1898794c
LucasM <lucas.mpt@ufu.br>
2023-03-22 23:06:52 -0300

    Exercicio 1 lista 2 feito

---
+++ LAB02 - First OOP code/ex01/ex01/src/Figurinha.java
@@ -0,0 +1,10 @@
+public class Figurinha {
+    String nome;
+    String nasc;
+    double altura;
+    int peso;
+    String pos;
+    String pais;
+
+
+}
---
+++ LAB02 - First OOP code/ex01/ex01/src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Figurinha neymar = new Figurinha();
+        neymar.nome = "Neymar Junior";
+        neymar.nasc = "05/02/1992";
+        neymar.altura = 1.75;
+        neymar.peso = 68;
+        neymar.pos = "atacante";
+        neymar.pais = "Brasil";
+
+        Figurinha rich = new Figurinha();
+        rich.nome = "Richarlison";
+        rich.nasc = "10/05/1997";
+        rich.altura = 1.84;
+        rich.peso = 83;
+        rich.pos = "atacante";
+        rich.pais = "Brasil";
+
+        Figurinha messi = new Figurinha();
+        messi.nome = "Lionel Messi";
+        messi.nasc = "24/06/1987";
+        messi.altura = 1.70;
+        messi.peso = 83;
+        messi.pos = "atacante";
+        messi.pais = "Argentina";
+
+        //printa neymar
+        System.out.println("Figurinha 1: " + neymar.nome + " " + neymar.nasc + " " + neymar.altura + " " + neymar.peso + " " + neymar.pos + " " + neymar.pais);
+
+        //printa richarlison
+        System.out.println("Figurinha 1: " + rich.nome + " " + rich.nasc + " " + rich.altura + " " + rich.peso + " " + rich.pos + " " + rich.pais);
+
+        //printa messi
+        System.out.println("Figurinha 1: " + messi.nome + " " + messi.nasc + " " + messi.altura + " " + messi.peso + " " + messi.pos + " " + messi.pais);
+    }
+}
+
